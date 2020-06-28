d09bd4f674d19fa6a50a73291b0349055c9fa5b1
Jedrzej Nowacki <contact@jnowacki.pl>
2020-06-28 12:46:08 +0200

    Redirections

--- src/main/java/pl/jnowacki/AnotherServlet.java
+++ src/main/java/pl/jnowacki/AnotherServlet.java
@@ -6,17 +6,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet("/servlet1")
-public class MyServlet extends HttpServlet {
+@WebServlet("/servlet2")
+public class AnotherServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Hello from servlet!");
+        // redirect:
+//        resp.sendRedirect(req.getContextPath() + "/servlet1");
+
+        //forward:
+//        getServletContext().getRequestDispatcher("/servlet1").forward(req, resp);
+
+        //include:
+//        getServletContext().getRequestDispatcher("/servlet1").include(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Hello from post!");
+        PrintWriter out = resp.getWriter();
+        out.println("Hello from post!");
     }
 }
