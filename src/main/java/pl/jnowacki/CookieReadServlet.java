239c75cb11fcd442e7d0f7bb03f0d2961a134213
Jedrzej Nowacki <contact@jnowacki.pl>
2020-06-28 14:59:56 +0200

    Task: cookies - ready

--- src/main/java/pl/jnowacki/CookieReadServlet.java
+++ src/main/java/pl/jnowacki/CookieReadServlet.java
@@ -7,12 +7,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
 
 @WebServlet("/myCookieRead")
 public class CookieReadServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getCookies();
+
+//        iteration
+        for(Cookie cookie: req.getCookies()) {
+            if(cookie.getName().equals("mojaNazwaCiasteczka")) {
+                resp.getWriter().printf("%s : %s", cookie.getName(), cookie.getValue());
+                return;
+            }
+        }
+
+//        by filter list
+
+//        Optional<Cookie> oCk = Arrays
+//                .stream(req.getCookies())
+//                .filter(cookie -> cookie.getName().equals("mojaNazwaCiasteczka"))
+//                .findFirst();
+//
+//        if(oCk.isPresent()) {
+//            resp.getWriter().println(oCk.get().getValue());
+//        } else {
+//            resp.getWriter().println("No cookie with that name!");
+//        }
     }
 }
